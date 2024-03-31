8da4158d47473d0e398126f86e115402c119ebaa
zli12321 <60415163+zli12321@users.noreply.github.com>
2024-03-31 00:31:28 -0400

    remove punctuation

--- qa_metrics/em.py
+++ qa_metrics/em.py
@@ -5,21 +5,22 @@ def em_match(reference, candidate):
     if isinstance(reference, list) and isinstance(candidate, list):
         reference = [remove_punctuation(normalize_answer(str(ele))) for ele in reference]
         candidate = [remove_punctuation(normalize_answer(str(ele))) for ele in candidate]
-
-        return bool(set(reference) & set(candidate))
     elif isinstance(reference, list):
         reference = [remove_punctuation(normalize_answer(str(ele))) for ele in reference]
         candidate = remove_punctuation(normalize_answer(str(candidate)))
-        return bool(set(reference) & set([candidate]))
     elif isinstance(candidate, list):
         candidate = [remove_punctuation(normalize_answer(str(ele))) for ele in candidate]
         reference = remove_punctuation(normalize_answer(str(reference)))
-        return bool(set([reference]) & set(candidate))
     else:
         '''
         Normalize the strings
         '''
-        reference = remove_punctuation(normalize_answer(str(reference)))
-        candidate = remove_punctuation(normalize_answer(str(candidate)))
+        reference = [remove_punctuation(normalize_answer(str(reference)))]
+        candidate = [remove_punctuation(normalize_answer(str(candidate)))]
 
-        return reference == candidate
+    for ref in reference:
+        for can in candidate:
+            if ref in can:
+                return True
+            
+    return False
