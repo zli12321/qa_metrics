f314d49495280bcef0fe4cc69bd43127ac24bee3
zli12321 <60415163+zli12321@users.noreply.github.com>
2024-04-01 20:11:45 -0400

    adding LLM prompting

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name='qa_metrics',
-    version='0.1.56',
+    version='0.2.4',
     author='Zongxia Li',
     author_email='zli12321@umd.edu',
     description='This package provides standard and classifier-based short form QA evaluation methods',
